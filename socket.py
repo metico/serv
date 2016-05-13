29775be4691aa30284ebedc142205f64fb677b34
M.Huzei <metico23@gmail.com>
2016-05-13 13:16:46 +0300

    echo serv

---
+++ socket.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(1)
+while True:
+    conn, addr = s.accept()
+    while True:
+            data = conn.recv(1024)
+            if data=='close' or data=='Close':
+                break
+            if not data: 
+		break
+            conn.send(data)
+    conn.close()
